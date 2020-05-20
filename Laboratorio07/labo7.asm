06d87f6821801a3912c4a4d7030abc36b28fa39f
María Lourdes <37163754+narommm@users.noreply.github.com>
2020-05-20 13:15:58 -0600

    Diferentes filas y columnas

--- Laboratorio07/labo7.asm
+++ Laboratorio07/labo7.asm
@@ -4,6 +4,8 @@ section .text
 	    call 	texto	
 	    call 	cursor
 	    call 	phrase
+	    call 	phrase1
+	    call 	phrase2
 	    call	kbwait
 
         int 	20h
@@ -27,38 +29,48 @@ w_char:	mov 	ah, 09h ;RESPECTIVO ATRIBUTO EN LA POSICION ACTUAL DEL CURSOR
 	int 	10h
 	ret
 
+w_char1:mov 	ah, 09h ;RESPECTIVO ATRIBUTO EN LA POSICION ACTUAL DEL CURSOR
+	mov 	al, al	;ASCII 
+	mov 	bh, 0h	;NUMERO DE PAGINA
+	mov 	bl, 00001111b  ;ATRIBUTO DEL CARACTER
+	mov 	cx, 1h ;CANTIDAD DE VECES QUE SE MUESTRA EL CARACTER
+	int 	10h
+	ret
+
+w_char2:mov 	ah, 09h ;RESPECTIVO ATRIBUTO EN LA POSICION ACTUAL DEL CURSOR
+	mov 	al, dl	;ASCII 
+	mov 	bh, 0h	;NUMERO DE PAGINA
+	mov 	bl, 00001111b  ;ATRIBUTO DEL CARACTER
+	mov 	cx, 1h ;CANTIDAD DE VECES QUE SE MUESTRA EL CARACTER
+	int 	10h
+	ret	
 kbwait: mov 	ax, 0000h ;PERMITE TENER LIMPIA LA PANTALLA
 	int 	16h
 	ret
 
 m_cursr:mov 	ah, 02h	;POSICION DEL CURSOR
 	mov 	dx, di  ; columna dx permite acceder a dl por la cantidad de bit que logra almacenar, para la posicion de la primera palabra
-	mov 	dh, 12d ; fila
+	mov 	dh, 1d ; fila
 	mov 	bh, 0h ;NUMERO PAGINA
 	int 	10h
 	ret
 
 m_cursr1:mov 	ah, 02h	;POSICION DEL CURSOR
-	mov 	dx, si  ; columna dx permite acceder a dl por la cantidad de bit que logra almacenar, para la posicion de la primera palabra
-	mov 	dh, 13d ; fila
+	mov 	dx, di  ; columna dx permite acceder a dl por la cantidad de bit que logra almacenar, para la posicion de la primera palabra
+	mov 	dh, 10d ; fila
 	mov 	bh, 0h ;NUMERO PAGINA
 	int 	10h
 	ret
 
 m_cursr2:mov 	ah, 02h	;POSICION DEL CURSOR
 	mov 	dx, di  ; columna dx permite acceder a dl por la cantidad de bit que logra almacenar, para la posicion de la primera palabra
-	mov 	dh, 14d ; fila
+	mov 	dh, 16d ; fila
 	mov 	bh, 0h ;NUMERO PAGINA
 	int 	10h
 	ret
 
-m_fila: mov ah, 05h
-	mov al, 14d
-
-phrase:	mov 	di, 29d
-	mov 	si, 29d
-
-lupi:	mov 	cl, [msg+di-29d]
+phrase: mov 	di, 10d
+lupi:	mov 	cl, [msg+di-10d]
 	call    m_cursr
 	call 	w_char
 	inc	di
@@ -66,28 +78,29 @@ lupi:	mov 	cl, [msg+di-29d]
 	jb	lupi
 	ret
 
-lupi1:	mov 	cl, [msg1+si-29d]
+phrase1: mov 	di, 29d
+lupi1:	mov 	al, [msg1+di-29d]
 	call    m_cursr1
-	call 	w_char
-	inc	si
-	cmp 	si, len1
+	call 	w_char1
+	inc	di
+	cmp 	di, len1
 	jb	lupi1
 	ret
 
-lupi2:	mov 	cl, [msg2+di-29d]
+phrase2: mov 	di, 12d
+lupi2:	mov 	dl, [msg1+di-12d]
 	call    m_cursr2
-	call 	w_char
-	call	m_fila
+	call 	w_char2
 	inc	di
 	cmp 	di, len2
-	jb	lupi1
-	ret	
+	jb	lupi2
+	ret
 section .data
 
 msg	db 	"Mami que tu quiere? "
 msg1	db	"PAPIIII SIIIIII"
 msg2	db	"Ver lo que esconde el pantalon"
-len 	equ	$-msg+29d
+len 	equ	$-msg+10d
 len1 	equ	$-msg1+29d
-len2 	equ	$-msg2+29d
+len2 	equ	$-msg2+12d
 
